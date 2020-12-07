29dc318b6bdb811db1c75c034fdb7362e1180005
Huseyingulecen <gulecenh45@gmail.com>
2020-12-07 14:45:24 +0100

    Initial Commit

---
+++ math-exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  math-exercise
+//
+//  Created by Hüseyin GÜLECEN on 7.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
